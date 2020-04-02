c463dfcae83ae6498ce4b643248b9f171275e28f
Alejo Gschwind <alejogschwind.97@gmail.com>
2020-04-02 19:02:38 -0300

    added basic Header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './header.styles.scss';
+
+const Header = () => (
+  <div className='header'>
+    <div className="top-header">
+      <div className='menu-btn'>
+        <svg width="27" height="22" viewBox="0 0 27 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <line y1="1" x2="20" y2="1" stroke="white" strokeWidth="2"/>
+          <line y1="10" x2="27" y2="10" stroke="white" strokeWidth="2"/>
+          <line x1="7" y1="19" x2="27" y2="19" stroke="white" strokeWidth="2"/>
+        </svg>
+      </div>
+      <div className='user-info'>
+        <span className='display-name'>Alejo G</span>
+        <div className="avatar"></div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Header from '../../components/header/header.component';
 import CardList from '../../components/card-list/card-list.component';
+
 
 import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className="homepage">
+    <Header />
     <CardList />
   </div>
 );
